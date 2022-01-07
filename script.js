6fc465119f6b85d891b7b326f38db7b4792f74be
Mauricio André Peraltilla Cuadros <peraltilla93@hotmail.com>
2022-01-07 14:03:27 -0500

    primer commit

---
+++ script.js
@@ -0,0 +1,18 @@
+//FizzBuzz
+function fizzBuzz (limite){
+    for (let i = 1; i <= limite; i++){
+        if ( (i % 3 === 0) && ( i % 5 === 0)){
+        console.log("FizzBuzz")
+        }
+        else if ( i % 3 === 0) {
+            console.log("Fizz");
+        }
+        else if (i % 5 === 0){
+        console.log("Buzz");
+        }
+        else
+        console.log(i);
+    }
+}
+
+fizzBuzz (100);
